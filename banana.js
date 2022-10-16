ab17957e342daab96e861d39e3c04c1c8e75f314
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-16 08:26:25 +0530

    fetch function added

--- banana.js
+++ banana.js
@@ -1,9 +1,21 @@
-var btnTextarea = document.querySelector(".textarea");
+var textArea = document.querySelector(".textarea");
 var btnTranslate = document.querySelector("#translate-btn");
+var outputArea = document.querySelector("#output");
 
 
-function clickHandler(){
-    console.log("clicked");
-};
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslation(text) {
+    return url + "?" + "text=" + text
+}
+
+function clickHandler() {
+    var inputText = textArea.value;
+    fetch(getTranslation(inputText))
+        .then(response => response.json())
+        .then(json => {
+            outputArea.innerText = json.contents.translated;
+        })
+}
 
 btnTranslate.addEventListener("click", clickHandler);
