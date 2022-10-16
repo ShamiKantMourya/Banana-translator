45ea7f55a0cfab8325669c200d58efc926707c22
BlackxJoker <88420924+BlackxJoker@users.noreply.github.com>
2022-10-16 07:06:00 +0530

    docs:jas file for translator

---
+++ banana.js
@@ -0,0 +1,9 @@
+var btnTextarea = document.querySelector(".textarea");
+var btnTranslate = document.querySelector("#translate-btn");
+
+
+function clickHandler(){
+    console.log("clicked");
+};
+
+btnTranslate.addEventListener("click", clickHandler);
